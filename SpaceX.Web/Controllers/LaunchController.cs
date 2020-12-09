1b29caa880b00b2a8d669b936ecdb2b2bd291eb6
Toma Bonev <tomabonev@yahoo.com>
2020-12-09 20:42:22 +0200

    Launch controller created. API Get request successfully returns data from SpaceX API for all launches.

---
+++ SpaceX.Web/Controllers/LaunchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using SpaceX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceX.Web.Controllers
+{
+    public class LaunchController : Controller
+    {
+        private readonly string getAllLaunchesUrl = "https://api.spacexdata.com/v3/launches";
+
+        public async Task<ICollection<LaunchPlanModel>> GetAll()
+        {
+            var client = new RestClient($"{getAllLaunchesUrl}");
+            var request = new RestRequest($"{getAllLaunchesUrl}", Method.GET);
+
+            IRestResponse response = client.Execute(request);
+
+            var launchList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<LaunchPlanModel>>(response.Content);
+
+            return launchList;
+        }
+    }
+}
