458d3970c47a5684f12030bfbae57ea28d1dbd20
Toma Bonev <tomabonev@yahoo.com>
2020-12-13 16:56:05 +0200

    Error handling added and styling updated.

---
+++ SpaceX.Web/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceX.Web.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await this.next(httpContext);
+
+            httpContext.Response.Redirect("Home/Error");
+        }
+    }
+}
--- SpaceX.Web/Startup.cs
+++ SpaceX.Web/Startup.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SpaceX.Services;
 using SpaceX.Services.Contracts;
+using SpaceX.Web.Middleware;
 
 namespace SpaceX.Web
 {
@@ -28,13 +30,11 @@ namespace SpaceX.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
             }
 
             app.UseStaticFiles();
